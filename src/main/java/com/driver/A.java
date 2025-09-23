c5725a13a73b170088630412b26d96f256ddac10
Kishor Kumar <kishorpatel1606@gmail.com>
2025-09-23 11:40:47 +0530

    package file added

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -1,3 +1,4 @@
+package com.driver;
 public class A {
     String  meth(){
         return "Invoking method from class A";
--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,3 +1,4 @@
+package com.driver;
 public class B extends A{
     String  meth(){
         return "Method is overridden in Extended class B";
