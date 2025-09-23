8493ca8b4dccd774485be6c06d6971a4fdff5341
Kishor Kumar <kishorpatel1606@gmail.com>
2025-09-23 11:32:27 +0530

    package error fixed

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,3 +1,4 @@
+package com.driver;
 class A {
     String  meth(){
         return "Invoking method from class A";
--- src/main/java/B.java
+++ src/main/java/B.java
@@ -1,3 +1,4 @@
+package com.driver;
 class B extends A{
     String  meth(){
         return "Method is overridden in Extended class B";
