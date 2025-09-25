79991f2a2599cf37ce92fa8deb8cabb1cc58d1eb
Kishor Kumar <kishorpatel1606@gmail.com>
2025-09-25 11:06:43 +0530

    resolved the error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,11 +2,25 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args){
-      B b = new B();
-      String ans = b.meth();
-      System.out.println(ans);
+      B obj1 = new B();
+      String ans1 = obj1.meth();
+      B obj2 = new B();
+      String ans2 = obj2.meth();
+      System.out.println(ans2);
 
   }
+    public class A {
+        public String  meth(){
+            return "Invoking method from class A";
+        }
+
+
+    }
+    public class B extends A{
+        public  String  meth(){
+            return "Method is overridden in Extended class B";
+        }
+    }
 
 }
 
